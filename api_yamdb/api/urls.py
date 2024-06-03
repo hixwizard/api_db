94f0bad303849c59333d6a87abc2993f7550d8c7
hix <stanislavskibarinov0@gmail.com>
2024-06-03 17:53:29 +0300

    base urls & views

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,34 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from .views import (
+    AuthViewSet, UserViewSet, TitleViewSet, CategoryViewSet,
+    GenreViewSet, ReviewViewSet, CommentViewSet
+)
+
+router_v1 = DefaultRouter()
+
+# Аутентификация
+router_v1.register('auth', AuthViewSet, basename='auth')
+
+# Пользователи
+router_v1.register('users', UserViewSet, basename='users')
+
+# Произведения
+router_v1.register('titles', TitleViewSet, basename='titles')
+
+# Категории
+router_v1.register('categories', CategoryViewSet, basename='categories')
+
+# Жанры
+router_v1.register('genres', GenreViewSet, basename='genres')
+
+# Отзывы
+router_v1.register('reviews', ReviewViewSet, basename='reviews')
+
+# Комментарии
+router_v1.register('comments', CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
