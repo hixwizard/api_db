7b1d462c03c7d9e651362a2d1ece8dbe976baab2
hix <stanislavskibarinov0@gmail.com>
2024-06-04 17:47:38 +0300

    Поправлена модель юзер

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,17 +1,33 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from core.constants import USERNAME_MAX_LENGTH, EMAIL_MAX
 
+USER = 'user'
+ADMIN = 'admin'
+MODERATOR = 'moderator'
 
-class UserModel(AbstractBaseUser):
+ROLE_CHOICES = [
+    (USER, USER),
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+]
+
+
+class UserModel(AbstractUser):
     """Кастомная модель пользователя."""
     username = models.CharField(max_length=USERNAME_MAX_LENGTH, unique=True)
     email = models.EmailField(max_length=EMAIL_MAX)
     first_name = models.CharField(max_length=USERNAME_MAX_LENGTH)
     last_name = models.CharField(max_length=USERNAME_MAX_LENGTH)
     bio = models.TextField('Биография', blank=True)
-    role = models.CharField(default='user')
+    role = models.CharField(
+        'роль',
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True
+    )
 
     class Meta:
         verbose_name = 'Пользователь'
