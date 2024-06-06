bbb991c70efcb1e032020a3d839a6d659a6653b9
hix <stanislavskibarinov0@gmail.com>
2024-06-06 15:23:10 +0300

    доп условия в users

--- api_yamdb/core/constants.py
+++ api_yamdb/core/constants.py
@@ -2,7 +2,7 @@ NAME_MAX = 256
 SLUG_MAX = 50
 USERNAME_MAX_LENGTH = 150
 MIN_CODE = 1000
-MAX_CODE = 9999
+MAX_CODE = 99999
 CODE_LENGTH = 4
 EMAIL_MAX = 254
 
@@ -13,3 +13,4 @@ ROLE_CHOICES = {
 }
 
 ROLE_CHOICES_LIST = [(key, value) for key, value in ROLE_CHOICES.items()]
+MESSAGE = 'Имя не должно содержать недопустимых символов.'
