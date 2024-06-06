2b86ed9be720bd341e4357d470bdee8ab0d45a63
Сергей Баданов <spy02m@gmail.com>
2024-06-06 16:27:12 +0400

    Исправления warning

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -18,6 +18,7 @@ class Category(models.Model):
     )
 
     class Meta:
+        ordering = ('id',)
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
@@ -38,6 +39,7 @@ class Genre(models.Model):
     )
 
     class Meta:
+        ordering = ('id',)
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
 
@@ -71,6 +73,7 @@ class Title(models.Model):
     )
 
     class Meta:
+        ordering = ('id',)
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
 
