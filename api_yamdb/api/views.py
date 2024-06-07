9e1edc81911a7f2bdb78122cda8eeb8b6ba5caee
Сергей Баданов <spy02m@gmail.com>
2024-06-07 23:35:04 +0400

    Успешные тесты по title, genre, category

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
@@ -5,8 +6,9 @@ from reviews.models import (
     Category, Genre, Title, Reviews, Comment
 )
 from .serializers import (
-    CategorySerializer, GenreSerializer, TitleSerializer, ReviewsSerializer, CommentSerializer
+    CategorySerializer, GenreSerializer, ReviewsSerializer, CommentSerializer, TitleGetSerializer, TitlePostSerializer
 )
+from .filters import TitleFilter
 from .mixins import CreateListDestroyViewSet
 from .permissons import AdminOrReadOnly
 
@@ -19,11 +21,19 @@ class AuthViewSet(viewsets.ModelViewSet):
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
-    serializer_class = TitleSerializer
     permission_classes = (AdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ('category', 'genre', 'name', 'year')
+    filterset_class = TitleFilter
+    http_method_names = ('get', 'post', 'patch', 'delete')
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return TitleGetSerializer
+        return TitlePostSerializer
+
+    def get_queryset(self):
+        return Title.objects.all().annotate(
+            rating=Avg('reviews__score')).order_by('id')
 
 
 class CategoryViewSet(CreateListDestroyViewSet):
