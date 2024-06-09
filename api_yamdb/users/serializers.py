d51a333c8213d5eac9aedeb908decdcc64a04789
hix <stanislavskibarinov0@gmail.com>
2024-06-10 01:35:37 +0300

    рабочие ручки auth tokenб добавить проверки

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -7,63 +7,37 @@ from django.core.validators import RegexValidator
 
 from .mixins import ExtraKwargsMixin
 from .models import UserModel
-from core.constants import USERNAME_MAX_LENGTH, MAX_CODE, EMAIL_MAX, MESSAGE
+from core.constants import USERNAME_MAX_LENGTH, MAX_CODE, EMAIL_MAX, MESSAGE, CODE_LENGTH
 
 
 class SignupSerializer(serializers.ModelSerializer):
-    """Сериализатор для регистрации новых пользователей."""
-    email = serializers.EmailField()
-    username = serializers.RegexField(
-        regex=r'^[\w.@+-]+$',
+    email = serializers.EmailField(max_length=254)  # Ensure email length is checked
+    username = serializers.CharField(
         max_length=USERNAME_MAX_LENGTH,
-        error_messages={
-            'invalid': MESSAGE
-        }
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+$',
+            message='Имя пользователя должно содержать только буквы, цифры, и символы @/./+/-/_',
+        )]
     )
 
     class Meta:
         model = UserModel
         fields = ('email', 'username')
 
-    def validate_username(self, value):
-        if value.lower() == 'me':
-            raise ValidationError('Имя "me" уже занято.')
-        return value
-
-    def validate_email(self, value):
-        if len(value) > EMAIL_MAX:
-            raise ValidationError(
-                'Email не может быть длиннее 254 символов.'
-            )
-        return value
-
     def validate(self, data):
         if UserModel.objects.filter(email=data['email']).exists():
-            raise ValidationError('Такая почта уже используется.')
+            raise serializers.ValidationError('Такая почта уже используется.')
         if UserModel.objects.filter(username=data['username']).exists():
-            raise ValidationError('Имя пользователя занято.')
+            raise serializers.ValidationError('Имя пользователя занято.')
         return data
 
 
 class TokenSerializer(serializers.Serializer):
-    """Сериализатор для создания токена аутентификации."""
     username = serializers.CharField()
-    confirmation_code = serializers.IntegerField(
-        max_value=MAX_CODE, required=True
-    )
+    confirmation_code = serializers.IntegerField(max_value=MAX_CODE)
 
     def create(self, validated_data):
-        username = validated_data['username']
-        confirmation_code = validated_data['confirmation_code']
-        try:
-            user = UserModel.objects.get(username=username)
-        except UserModel.DoesNotExist:
-            raise ValidationError(
-                'Пользователь с указанным именем не найден'
-            )
-        cached_code = cache.get(f'confirmation_code_{user.email}')
-        if cached_code != str(confirmation_code):
-            raise ValidationError('Неверный код подтверждения')
+        user = UserModel.objects.get(username=validated_data['username'])
         refresh = RefreshToken.for_user(user)
         return {
             'refresh': str(refresh),
