0ea0062b762b2066a43e5f6bf5cd7625018e4453
Stanislav Barinov <152168070+hix9@users.noreply.github.com>
2024-06-10 19:42:56 +0300

    Update urls.py
    прокинул пути правильно

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -13,10 +13,11 @@ router_v1 = DefaultRouter()
 router_v1.register('titles', TitleViewSet, basename='titles')
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
-router_v1.register(r'titles(?P<title_id>\d+)/reviews',
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
                    ReviewViewSet, basename='reviews')
-router_v1.register(r'reviews(?P<review_id>\d+)/comments',
-                   CommentViewSet, basename='comments')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments',
+    CommentViewSet, basename='comments')
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
     path('v1/', include('users.urls')),
