c185bca24bc393ee153f663f0d7f29fc949210bf
Stanislav Barinov <152168070+hix9@users.noreply.github.com>
2024-06-13 13:57:45 +0300

    Update permissons.py

--- api_yamdb/api/permissons.py
+++ api_yamdb/api/permissons.py
@@ -11,20 +11,15 @@ class AdminOrReadOnly(BasePermission):
                 and request.user.is_admin or request.user.is_superuser)
 
 
-class ReviewCommentPermission(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+class IsAuthorIsModeratorIsAdminOrReadOnly(permissions.BasePermission):
+    """Доступ автору или модератору или только для чтения."""
 
     def has_object_permission(self, request, view, obj):
-        if (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        ):
-            return True
-        return (
-            request.user.is_authenticated
-            and (request.user.is_moderator or request.user.is_admin)
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                and (request.user == obj.author
+                     or request.user.role == 'moderator'
+                     or request.user.is_admin
+                     or request.user.is_superuser
+                     )
+                )
