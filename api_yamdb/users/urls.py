12a37348c5a04b7c70405d70aba52ee360bdd65a
Stanislav Barinov <152168070+hix9@users.noreply.github.com>
2024-06-17 19:36:19 +0300

    Update urls.py

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,15 +1,1 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
 
-from users.views import SignupView, TokenView, UserViewSet
-
-router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
-
-app_name = 'users'
-
-urlpatterns = [
-    path('auth/signup/', SignupView.as_view(), name='signup'),
-    path('auth/token/', TokenView.as_view(), name='token'),
-    path('', include(router.urls)),
-]
