e64761cee84274a3a0b53eb83cde3052560c14e7
Stanislav Barinov <152168070+hix9@users.noreply.github.com>
2024-06-18 13:04:48 +0300

    import

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,6 @@ import random
 from django.core.mail import send_mail
 from django.utils.timezone import now
 from rest_framework import serializers
-from rest_framework.serializers import ValidationError
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.core.validators import RegexValidator
